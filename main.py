f3a542dce983210b2e3619eb00ebc1f1ddfff0cb
Greatshot59 <avilaricardo360@gmail.com>
2022-02-01 23:29:47 -0800

    Deciding BEST Data Collection Type For User Bills

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+### User Input ###
+# Starting Amount
+# Income Amount and Payday
+# Bill Amount and Due Dates
+# Date to Simulate to
+
+userInput_Income = input("Income (x.xx): $")
+userInput_PayDay = input("Payday (12,31...):  ")
+
+# Whats the BEST data collection type for storing bills?
+
+### SAMPLE DATA ###
+# due date(date) : amount(float)
+# 1 : 700
+# 10 : 125
+# 14 : 14.99
+# 30 : 20
+
+userInput_Bills = {}
+
+    
+
+
+
 ### OUTLINE of Project ###
 
 # Track Starting Income && Paydays
@@ -7,11 +31,6 @@
 # Progress Time
 #     If day is equal to the day of Payday OR Bill Due, add OR deduct X amount.
 
-### User Input ###
-# Starting Amount
-# Income Amount and Payday
-# Bill Amount and Due Dates
-# Date to Simulate to
 
 ### Program Output ###
 # Amount in "Account" After Simulation
