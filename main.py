7821d66c99c97a209b0ab462d34a43995640bfd7
Greatshot59 <avilaricardo360@gmail.com>
2022-02-01 23:05:00 -0800

    The Outline to the Project Established

---
+++ main.py
@@ -0,0 +1,17 @@
+### OUTLINE of Project ###
+
+# Track Starting Income && Paydays
+
+# Track Bills & Due Dates
+
+# Progress Time
+#     If day is equal to the day of Payday OR Bill Due, add OR deduct X amount.
+
+### User Input ###
+# Starting Amount
+# Income Amount and Payday
+# Bill Amount and Due Dates
+# Date to Simulate to
+
+### Program Output ###
+# Amount in "Account" After Simulation
