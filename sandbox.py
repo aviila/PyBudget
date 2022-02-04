b782d685414b745d8342fa59f9f2f17dc34645a3
Greatshot59 <avilaricardo360@gmail.com>
2022-02-03 20:27:08 -0800

    Working Bill Handler

--- sandbox.py
+++ sandbox.py
@@ -7,17 +7,14 @@ sampleList = []
 while tag == True:
     dict = {}
     key = input("name of bill: ")
-    value = input("amount: ")
-
-    dict.update({key,value})
-
     if key == "done":
         tag = False
-    elif value == "done":
-        tag = False
-    else:
-        print(key)
-        print(value)
-        sampleList.append(dict)
 
-print(sampleList)
+    value = input("amount: ")
+    if value == "done":
+        tag = False
+    dict.update({str(key):int(value)})
+
+    print("\n")
+    sampleList.append(dict)
+    print(sampleList)
