d4a9eae0be8e5f4ece941576aee04fc4a3890e10
Greatshot59 <avilaricardo360@gmail.com>
2022-02-16 00:25:06 -0800

    Calculating Changes in Time & Resolve Input Errors

--- sandbox.py
+++ sandbox.py
@@ -1,31 +1,29 @@
-### How Date Simulation is going to work ###
-from datetime import date, datetime, timedelta
+while True:
+    userInput = int(input("Date Here: "))
 
-#For Computer Use
-compTodayDay = date.today()
-
-#For Human Use (comp -> human)
-humanTodayDay = compTodayDay.strftime("%B %d, %Y")
-
-print(compTodayDay.day)
-print(humanTodayDay)
-print("\n")
-
-#For Human Use
-userDate = "February 10 2022"
-trackingDay = "Mar 15 2022"
-#For Computer Use (human -> comp)
-compUserDate = datetime.strptime(userDate, '%B %d %Y').date()
-compTrackingDay = datetime.strptime(trackingDay, '%b %d %Y').date()
-
-# if compTrackingDay > compUserDate:
+    if userInput != int:
+        print("Not Integer Try Again")
 
 
-print(compUserDate)
-print(compTrackingDay)
-print("\n")
 
-### Bake Bill / Payday in While Loop ###
-while compUserDate <= compTrackingDay:
-    print(compUserDate.strftime("%B %d, %Y"))
-    compUserDate = compUserDate + timedelta(days=1)
+
+
+
+
+# Try and Calculate timeDelta
+# from datetime import datetime, date, timedelta
+# from tracemalloc import start
+
+
+# startDate = date.today()
+# timeCount = 0
+# endDate = date(2022, 3, 15)
+
+# while startDate <= endDate:
+#     print(startDate)
+#     startDate += timedelta(1)
+#     timeCount += 1
+#     if timeCount % 7 == 0:
+#         print("Days Passed: {} Payday!".format(timeCount) + "\n")
+
+# print(timeCount)
